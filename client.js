400e80eebc2c14206677f5e850b2b68a488100dc
NTTKDeveloper <nttkct2020@gmail.com>
2023-05-27 08:47:23 +0700

    Update ClientGame

--- client.js
+++ client.js
@@ -5,6 +5,15 @@ const socket = io("http://localhost:3000");
 
 // client-side
 socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
 });
+
+socket.on('disconnect', function() {
+    console.log('Disconnected');
+});
+
+socket.on("Hello", (arg) => {
+    console.log(arg);
+});
+
 
